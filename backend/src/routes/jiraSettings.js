e078b6273e35b302e979dc60040b878dca1eeb47
aliamani66 <aliamani66@gmail.com>
2026-08-11 22:20:57 +0330

    fix: import https module in jiraSettings.js to fix https is not defined error

--- backend/src/routes/jiraSettings.js
+++ backend/src/routes/jiraSettings.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const jiraMapping = require('../jiraMapping');
